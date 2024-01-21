b77e841257788c478241b97ebddf33c58aa4473c
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-21 19:41:50 +0100

    added bonus

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const { createApp } = Vue;
 const app = createApp({
     data(){
         return {
-            title: 'Hello World'
+            title: 'Hello World',
+            image: 'https://learn.microsoft.com/it-it/shows/hello-world/media/helloworld_383x215.png'
         }
     }
 });
